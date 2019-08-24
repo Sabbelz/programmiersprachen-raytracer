66d0af90a9987cbab22b2ec103bd16cfa2bbe1a0
Sabbelz <Sebastian.Reichmann@uni-weimar.de>
2019-08-24 18:02:19 +0200

    HALP!

--- framework/composite.hpp
+++ framework/composite.hpp
@@ -17,7 +17,7 @@ class Composite : public Shape {
 
     void createBoundingBox();
     void add(std::shared_ptr<Shape> shape);
-    void add(std::shared_ptr<Composite> somposite);
+    void add(std::shared_ptr<Composite> composite);
     std::vector<std::shared_ptr<Shape>> get_Shape(std::vector<std::shared_ptr<Shape>>& shapes);
     hitpoint intersect(Ray const& r) const override;
     float area() const override;
