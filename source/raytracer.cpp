7662ab6cbd389e90908db722a6aad418b7cc820b
Sabbelz <Sebastian.Reichmann@uni-weimar.de>
2019-09-01 19:51:22 +0200

    added sdf

--- source/raytracer.cpp
+++ source/raytracer.cpp
@@ -10,21 +10,24 @@ int main(int argc, char* argv[])
 {
   unsigned const image_width = 600;
   unsigned const image_height = 600;
-  std::string const filename = "Picture";
+  std::string const filename = "Animation";
 
   std::cout<<"reading sdf  \n";
-  // Scene scene = read_SDF("/home/sebastian/materials.sdf");
-  Scene scene = read_SDF("/home/luigi/Documents/Sebastian_raytracer/programmiersprachen-raytracer/SDF/materials.sdf");
+  Scene scene = read_SDF("/home/sebastian/materials.sdf");
+  //Scene scene = read_SDF("/home/luigi/Documents/Sebastian_raytracer/programmiersprachen-raytracer/SDF/materials.sdf");
   
   std::cout<<"reading sdf finished \n";
   Light light1("Licht", {0.0f,0.0f,0.0f}, {1.0f,1.0f,1.0f}, 0.0);
   Light light2("Licht2", {100.0f,100.0f,50.0f}, {1.0f,1.0f,1.0f}, 1.0);
+  Light light3("Licht3", {-6.0f, 30.0f, 10.0f}, {1.0f,1.0f, 1.0f}, 1.0);
   Ambiente amb{{0.93333f,0.93333f,0.0f}};
   Camera cam("Kamera", {0.0f,0.0f,0.0f},{0.0f,0.0f,-1.0f},{0.0f,1.0f,0.0f},110);
   auto ptr_light = std::make_shared<Light>(light1);
   auto ptr_light2 = std::make_shared<Light>(light2);
+  auto ptr_light3 = std::make_shared<Light>(light3);
   scene.light_.push_back(ptr_light);
   scene.light_.push_back(ptr_light2);
+  scene.light_.push_back(ptr_light3);
 
   Renderer renderer{image_width, image_height, filename, scene};
 
